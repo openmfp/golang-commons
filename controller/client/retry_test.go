651dd3d8392ea18b2ba61837902c372ca5332eba
Bastian Echterhölter <bastian.echterhoelter@sap.com>
2024-03-25 16:37:53 +0100

    a few more negative cases

--- controller/client/retry_test.go
+++ controller/client/retry_test.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"testing"
 
-	"github.com/openmfp/golang-commons/controller/testSupport"
 	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/api/errors"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/openmfp/golang-commons/controller/testSupport"
 )
 
 func TestRetry(t *testing.T) {
@@ -40,5 +42,39 @@ func TestRetry(t *testing.T) {
 
 		// Assert
 		assert.NoError(t, err)
+	})
+
+	t.Run("Retry update and fail", func(t *testing.T) {
+		// Arrange
+		ctx := context.Background()
+		newObject := &testSupport.TestApiObject{
+			ObjectMeta: v1.ObjectMeta{Name: "test1", Namespace: "test1"},
+		}
+
+		// Act
+		err := RetryUpdate(ctx, func(object client.Object) client.Object {
+			return object
+		}, newObject, c)
+
+		// Assert
+		assert.Error(t, err)
+		assert.True(t, errors.IsNotFound(err))
+	})
+
+	t.Run("Retry Status update and fail", func(t *testing.T) {
+		// Arrange
+		ctx := context.Background()
+		newObject := &testSupport.TestApiObject{
+			ObjectMeta: v1.ObjectMeta{Name: "test1", Namespace: "test1"},
+		}
+
+		// Act
+		err := RetryStatusUpdate(ctx, func(object client.Object) client.Object {
+			return object
+		}, newObject, c)
+
+		// Assert
+		assert.Error(t, err)
+		assert.True(t, errors.IsNotFound(err))
 	})
 }
