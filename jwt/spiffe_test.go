5d380b281c6b79330b82e26e63d10729ef57f636
Tobias Oetzel <tobias.oetzel@sap.com>
2024-03-25 06:50:47 +0100

    add spiffe test

---
+++ jwt/spiffe_test.go
@@ -0,0 +1,30 @@
+package jwt
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSpiffe(t *testing.T) {
+	t.Run("GetSpiffeUrlValue", func(t *testing.T) {
+		// Arrange
+		header := make(map[string][]string)
+		header[HeaderSpiffeValue] = []string{"URI=spiffe://example.com"}
+
+		// Act
+		got := GetSpiffeUrlValue(header)
+
+		// Assert
+		assert.NotNil(t, got)
+		assert.Equal(t, "spiffe://example.com", *got)
+	})
+
+	t.Run("GetURIValueForNotMatchingRegEx", func(t *testing.T) {
+		// Act
+		got := GetURIValue("spiffe://example.com")
+
+		// Assert
+		assert.Equal(t, "", got)
+	})
+}
