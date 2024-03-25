eea635272337cdd7b3635d31783da093cad5f01d
Bastian Echterhölter <bastian.echterhoelter@sap.com>
2024-03-25 15:14:36 +0100

    adjusting sentry recover logs

--- context/sentry_test.go
+++ context/sentry_test.go
@@ -12,19 +12,28 @@ import (
 
 func TestRecover(t *testing.T) {
 	t.Parallel()
+	t.Run("should recover from panic and log", func(t *testing.T) {
 
-	log := testlogger.New()
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		defer Recover(log.Logger)
-		panic("test panic")
-	}()
-	wg.Wait()
+		log := testlogger.New()
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			defer Recover(log.Logger)
+			panic("test panic")
+		}()
+		wg.Wait()
 
-	logMessages, err := log.GetLogMessages()
-	assert.NoError(t, err)
-	require.Len(t, logMessages, 1)
-	assert.Equal(t, "recovered panic", logMessages[0].Message)
+		logMessages, err := log.GetLogMessages()
+		assert.NoError(t, err)
+		require.Len(t, logMessages, 1)
+		assert.Equal(t, "recovered panic", logMessages[0].Message)
+	})
+
+	t.Run("should recover from panic", func(t *testing.T) {
+		assert.NotPanics(t, func() {
+			defer Recover(nil)
+			panic("test panic")
+		})
+	})
 }
