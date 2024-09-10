fabfc6ba6f148ddd8d94cd866fd65378e9cc1ff4
Angel Kafazov <akafazov@cst-bg.net>
2024-09-10 10:25:14 +0300

    remove unused code, add tests

--- directive/config.go
+++ directive/config.go
@@ -1,20 +1,7 @@
 package directive
 
-import (
-	"log"
-
-	"github.com/vrischmann/envconfig"
-)
-
 type configuration struct {
 	DirectivesAuthorizationEnabled bool `envconfig:"default=false"`
 }
 
 var directiveConfiguration = configuration{}
-
-func init() {
-	err := envconfig.Init(&directiveConfiguration)
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
