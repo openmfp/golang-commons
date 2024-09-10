74c7314cc40b6bb2a23c86fa39e0b16801af8e94
Angel Kafazov <akafazov@cst-bg.net>
2024-09-10 15:53:25 +0300

    Increase code coverage

--- directive/authorized_test.go
+++ directive/authorized_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/go-jose/go-jose/v4"
 	openfgav1 "github.com/openfga/api/proto/openfga/v1"
 	openmfpcontext "github.com/openmfp/golang-commons/context"
+	"github.com/openmfp/golang-commons/context/keys"
 	logger "github.com/openmfp/golang-commons/logger/testlogger"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -195,7 +196,23 @@ func TestAuthorized(t *testing.T) {
 				s.EXPECT().Check(mock.Anything, mock.Anything).Return(&openfgav1.CheckResponse{Allowed: true}, nil)
 			},
 		},
+		{
+			name:            "ListStores returns error",
+			entityParamName: "existent",
+			entityType:      String("value"),
+			isTechnicalUser: true,
+			graphqlArgs: map[string]any{
+				"existent": "something",
+				"tenantId": "tenant-id",
+			},
+			fgaMocks: func(s *mocks.OpenFGAServiceClient) {
+				s.EXPECT().ListStores(mock.Anything, mock.Anything).Return(nil, errors.New("ListStores error"))
+				// s.EXPECT().Check(mock.Anything, mock.Anything).Return(&openfgav1.CheckResponse{Allowed: true}, nil)
+			},
+			expectedError: errors.New("ListStores error"),
+		},
 	}
+
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
 
@@ -261,5 +278,213 @@ func TestAuthorizedWithSpiffeeHeader(t *testing.T) {
 	nextFn := func(ctx context.Context) (interface{}, error) { return nil, nil }
 	_, err := Authorized(openfgaMock, logger.New().Logger)(ctx, nil, nextFn, "member", String("project"), nil, "projectId")
 	assert.NoError(t, err)
+
+}
+
+func TestAuthorizedEdgeCases2(t *testing.T) {
+	directiveConfiguration.DirectivesAuthorizationEnabled = true
+	testCases := []struct {
+		name                string
+		relation            string
+		entityID            string
+		entityParamName     string
+		entityType          *string
+		entityTypeParamName *string
+		isTechnicalUser     bool
+		graphqlArgs         map[string]any
+		fgaMocks            func(s *mocks.OpenFGAServiceClient)
+		expectedError       error
+	}{
+		{
+			name:            "Wrong key value for tenant in context",
+			entityParamName: "existent",
+			entityType:      String("value"),
+			isTechnicalUser: false,
+			graphqlArgs: map[string]any{
+				"existent": "something",
+				"tenantId": "tenant-id",
+			},
+			fgaMocks: func(s *mocks.OpenFGAServiceClient) {
+			},
+			expectedError: fmt.Errorf("someone stored a wrong value in the [tenantId] key with type [<nil>], expected [string]"),
+		},
+		{
+			name:            "Check() return error",
+			entityParamName: "existent",
+			entityType:      String("value"),
+			isTechnicalUser: true,
+			graphqlArgs: map[string]any{
+				"existent": "something",
+				"tenantId": "tenant-id",
+			},
+			fgaMocks: func(s *mocks.OpenFGAServiceClient) {
+				s.EXPECT().ListStores(mock.Anything, mock.Anything).Return(&openfgav1.ListStoresResponse{
+					Stores: []*openfgav1.Store{
+						{
+							Name: "tenant-tenant-id",
+						},
+					},
+				}, nil)
+				s.EXPECT().Check(mock.Anything, mock.Anything).Return(nil, errors.New("Check error"))
+			},
+			expectedError: errors.New("Check error"),
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+
+			openfgaMock := mocks.NewOpenFGAServiceClient(t)
+			if test.fgaMocks != nil {
+				test.fgaMocks(openfgaMock)
+			}
+
+			nextFn := func(ctx context.Context) (interface{}, error) { return nil, nil }
+
+			log := logger.New()
+
+			ctx := context.Background()
+			ctx = graphql.WithFieldContext(ctx, &graphql.FieldContext{
+				Args: test.graphqlArgs,
+			})
+
+			if test.isTechnicalUser {
+				ctx = openmfpcontext.AddIsTechnicalIssuerToContext(ctx)
+			} else {
+				ctx = context.WithValue(ctx, keys.WebTokenCtxKey, "tenantId")
+			}
+			ctx = openmfpcontext.AddWebTokenToContext(ctx, token, signatureAlgorithms)
+			ctx = openmfpcontext.AddAuthHeaderToContext(ctx, fmt.Sprintf("Bearer %s", token))
+
+			_, err := Authorized(openfgaMock, log.Logger)(ctx, nil, nextFn, test.relation, test.entityType, test.entityTypeParamName, test.entityParamName)
+			assert.Equal(t, test.expectedError, err)
+		})
+	}
+
+}
+
+func TestAuthorizedEdgeCases(t *testing.T) {
+	directiveConfiguration.DirectivesAuthorizationEnabled = false
+	testCases := []struct {
+		name                string
+		relation            string
+		entityID            string
+		entityParamName     string
+		entityType          *string
+		entityTypeParamName *string
+		isTechnicalUser     bool
+		graphqlArgs         map[string]any
+		fgaMocks            func(s *mocks.OpenFGAServiceClient)
+		expectedError       error
+	}{
+		{
+			name:            "should go to next() due to disabled directives configuration",
+			entityParamName: "existent",
+			entityType:      String("value"),
+			isTechnicalUser: true,
+			graphqlArgs: map[string]any{
+				"existent": "something",
+				"tenantId": "tenant-id",
+			},
+			fgaMocks: nil,
+		},
+	}
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+
+			openfgaMock := mocks.NewOpenFGAServiceClient(t)
+			if test.fgaMocks != nil {
+				test.fgaMocks(openfgaMock)
+			}
+
+			nextFn := func(ctx context.Context) (interface{}, error) { return nil, nil }
+
+			log := logger.New()
+
+			ctx := context.Background()
+			ctx = graphql.WithFieldContext(ctx, &graphql.FieldContext{
+				Args: test.graphqlArgs,
+			})
+
+			if test.isTechnicalUser {
+				ctx = openmfpcontext.AddIsTechnicalIssuerToContext(ctx)
+			} else {
+				ctx = openmfpcontext.AddTenantToContext(ctx, "tenant-id")
+			}
+			ctx = openmfpcontext.AddWebTokenToContext(ctx, token, signatureAlgorithms)
+			ctx = openmfpcontext.AddAuthHeaderToContext(ctx, fmt.Sprintf("Bearer %s", token))
+
+			_, err := Authorized(openfgaMock, log.Logger)(ctx, nil, nextFn, test.relation, test.entityType, test.entityTypeParamName, test.entityParamName)
+			assert.Equal(t, test.expectedError, err)
+		})
+	}
+
+	// test 2
+	directiveConfiguration.DirectivesAuthorizationEnabled = true
+	testCases = []struct {
+		name                string
+		relation            string
+		entityID            string
+		entityParamName     string
+		entityType          *string
+		entityTypeParamName *string
+		isTechnicalUser     bool
+		graphqlArgs         map[string]any
+		fgaMocks            func(s *mocks.OpenFGAServiceClient)
+		expectedError       error
+	}{
+		{
+			name:            "should go to next() due to disabled directives configuration",
+			entityParamName: "existent",
+			entityType:      String("value"),
+			isTechnicalUser: true,
+			graphqlArgs: map[string]any{
+				"existent": "something",
+				"tenantId": "tenant-id",
+			},
+			fgaMocks: func(s *mocks.OpenFGAServiceClient) {
+				s.EXPECT().ListStores(mock.Anything, mock.Anything).Return(&openfgav1.ListStoresResponse{
+					Stores: []*openfgav1.Store{
+						{
+							Name: "tenant-tenant-id",
+						},
+					},
+				}, nil)
+				s.EXPECT().ReadAuthorizationModels(mock.Anything, mock.Anything).Return(&openfgav1.ReadAuthorizationModelsResponse{
+					AuthorizationModels: []*openfgav1.AuthorizationModel{
+						{
+							Id: "id",
+						},
+					},
+				}, nil)
+				s.EXPECT().Check(mock.Anything, mock.Anything).Return(&openfgav1.CheckResponse{Allowed: false}, nil)
+			},
+			expectedError: errors.New("OpenFGAServiceClient is nil. Cannot process request"),
+		},
+	}
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+
+			nextFn := func(ctx context.Context) (interface{}, error) { return nil, nil }
+
+			log := logger.New()
+
+			ctx := context.Background()
+			ctx = graphql.WithFieldContext(ctx, &graphql.FieldContext{
+				Args: test.graphqlArgs,
+			})
+
+			if test.isTechnicalUser {
+				ctx = openmfpcontext.AddIsTechnicalIssuerToContext(ctx)
+			} else {
+				ctx = openmfpcontext.AddTenantToContext(ctx, "tenant-id")
+			}
+			ctx = openmfpcontext.AddWebTokenToContext(ctx, token, signatureAlgorithms)
+			ctx = openmfpcontext.AddAuthHeaderToContext(ctx, fmt.Sprintf("Bearer %s", token))
+
+			_, err := Authorized(nil, log.Logger)(ctx, nil, nextFn, test.relation, test.entityType, test.entityTypeParamName, test.entityParamName)
+			assert.Equal(t, test.expectedError.Error(), err.Error())
+		})
+	}
 
 }
